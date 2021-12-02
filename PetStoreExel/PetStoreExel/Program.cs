37cb748a44d4bdf65f2cab4f1545ca08e7497e40
Melvin.Mathew <Melvin.Mathew@ust.com>
2021-12-02 09:25:56 +0530

    first commit

---
+++ PetStoreExel/PetStoreExel/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using excel = Microsoft.Office.Interop.Excel;
+namespace TestPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            excel.Application xlapp = new excel.Application();
+            excel.Workbook wbook = xlapp.Workbooks.Open(@"C:\Users\DELL\Desktop\Data.xlsx");
+            excel.Worksheet xlworksheet = wbook.Sheets[1];
+            excel.Range xlrange = xlworksheet.UsedRange;
+            string url;
+            for (int i = 1; i <= 3; i++)
+            {
+                url = xlrange.Cells[i][1].value2;
+                IWebDriver driver = new ChromeDriver();
+                driver.Navigate().GoToUrl(url);
+                Thread.Sleep(3000);
+                driver.Close();
+
+            }
+        }
+    }
+}
---
+++ PetStoreExel/README.md.txt
@@ -0,0 +1,11 @@
+----------------------------------------------------------------------------------------------------------------------------------------
+
+Basic Demo For Selenium Data Driven Test using Excel
+
+----------------------------------------------------------------------------------------------------------------------------------------
+
+-------->Move the excel Sheet to the requird location
+
+-------->Change the Path accordingly in "excel.Workbook wbook = xlapp.Workbooks.Open(@"provide the path of Excel sheet here"):
+
+-------->Execute the program
